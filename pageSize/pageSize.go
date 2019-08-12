9e1481563bb8bf5d6d4f31b8f547be869cbc18f3
daixin <jzldaixin@gmail.com>
2019-08-12 16:28:49 +0800

    pageSize逻辑

--- pageSize/pageSize.go
+++ pageSize/pageSize.go
@@ -1,18 +1,24 @@
 package pageSize
 
 import (
-	"github.com/Unknwon/com"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
-func GetPageSize(page,size int) (int, int) {
+func GetPageSize(page, size int) (int, int) {
 	offset := (page - 1) * size
 	return offset, size
 }
 
 func GinPageSize(c *gin.Context) (int, int) {
-	page := com.StrTo(c.DefaultQuery("page", "1")).MustInt()
-	size := com.StrTo(c.DefaultQuery("size", "10")).MustInt()
-	offset := (page - 1) * size
-	return offset, size
+	page := c.DefaultQuery("page", "1")
+	size := c.DefaultQuery("size", "10")
+	p, err1 := strconv.Atoi(page)
+	s, err2 := strconv.Atoi(size)
+	if err1 != nil || err2 != nil {
+		p = 1
+		s = 10
+	}
+	offset := (p - 1) * s
+	return offset, s
 }
