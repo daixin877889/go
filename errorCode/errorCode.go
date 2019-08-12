03f6235049444897a545c571b0b7df420d6bf89a
daixin <jzldaixin@gmail.com>
2019-08-12 17:09:28 +0800

    errorCode

--- errorCode/errorCode.go
+++ errorCode/errorCode.go
@@ -6,5 +6,9 @@ type ErrData struct {
 }
 
 var (
+	ErrorInternalError = ErrData{Code: 50000, Msg: "内部错误"}
 	ErrorDBError = ErrData{Code: 50001, Msg: "数据库错误"}
+	ErrorParameterError = ErrData{Code: 40002, Msg: "参数错误"}
+	ErrorNotAuthUser = ErrData{Code: 40001, Msg: "鉴权失败"}
+	ErrorNoPermission = ErrData{Code: 40003, Msg: "无权限"}
 )
