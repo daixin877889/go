38e64015ea1b9b2deeaedecf1231c6ccdf16dcce
daixin <jzldaixin@gmail.com>
2019-08-12 21:42:53 +0800

    errorCode

--- errorCode/errorCode.go
+++ errorCode/errorCode.go
@@ -11,4 +11,5 @@ var (
 	ErrorParameterError = ErrData{Code: 40002, Msg: "参数错误"}
 	ErrorNotAuthUser = ErrData{Code: 40001, Msg: "鉴权失败"}
 	ErrorNoPermission = ErrData{Code: 40003, Msg: "无权限"}
+	ErrorNotfindrror = ErrData{Code: 40004, Msg: "资源不存在"}
 )
