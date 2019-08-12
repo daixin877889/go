2afc94f041d24e5710a83ac2f0fbf02720feb50a
daixin <jzldaixin@gmail.com>
2019-08-12 13:58:46 +0800

    pageSize

--- pageSize/pageSize.go
+++ pageSize/pageSize.go
@@ -1,6 +1,18 @@
 package pageSize
 
+import (
+	"github.com/Unknwon/com"
+	"github.com/gin-gonic/gin"
+)
+
 func GetPageSize(page,size int) (int, int) {
+	offset := (page - 1) * size
+	return offset, size
+}
+
+func GinPageSize(c *gin.Context) (int, int) {
+	page := com.StrTo(c.DefaultQuery("page", "1")).MustInt()
+	size := com.StrTo(c.DefaultQuery("size", "10")).MustInt()
 	offset := (page - 1) * size
 	return offset, size
 }
