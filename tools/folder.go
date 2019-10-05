0930228f53f8b53483a7ddf1aca2d771acf8fd1c
daixin <jzldaixin@gmail.com>
2019-10-05 15:03:08 +0800

    新建文件夹工具

---
+++ tools/folder.go
@@ -0,0 +1,43 @@
+package tools
+
+import (
+	"fmt"
+	"os"
+)
+
+// 判断文件夹是否存在
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+// 创建文件夹
+func CreateFolder(path string) bool {
+	_dir := path
+	exist, err := PathExists(_dir)
+	if err != nil {
+		fmt.Printf("get dir error![%v]\n", err)
+		return false
+	}
+	if exist {
+		fmt.Printf("has dir![%v]\n", _dir)
+	} else {
+		fmt.Printf("no dir![%v]\n", _dir)
+		// 创建文件夹
+		err := os.Mkdir(_dir, os.ModePerm)
+		if err != nil {
+			fmt.Printf("mkdir failed![%v]\n", err)
+			return false
+		} else {
+			fmt.Printf("mkdir success!\n")
+
+		}
+	}
+	return true
+}
